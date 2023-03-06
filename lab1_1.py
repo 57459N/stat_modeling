780d29555c63c7ce324d9118a3328dbae07d39f6
Stas Indyukov <stasun008@gmail.com>
2023-03-06 12:38:36 +0300

    lab1_1

--- lab1_1.py
+++ lab1_1.py
@@ -1,8 +1,17 @@
 import numpy as np
+import scipy
+from pprint import pprint
 
 
 def main():
-    pass
+    a = np.array([[5, -1],
+                  [1, -1]],
+                 dtype=float)
+    b = np.array([[14],
+                  [6]],
+                 dtype=float)
+
+    pprint(scipy.linalg.solve(a, b))
 
 
 if __name__ == "__main__":
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 Вариант 8
 #
-    lab1_1: A*X = B
+    lab1_1:
 
     A = 5 -1     
         1 -1 
@@ -9,7 +9,7 @@
          6
     
 #
-    lab1_2: y = ax^3 + bx^2 + c
+    lab1_2: y = ax<sup>3</sup> + bx<sup>2</sup> + c
 
     a = 1 
     b = c = 2
